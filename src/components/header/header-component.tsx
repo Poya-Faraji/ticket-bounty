aa085a93be5dd7ad5709305a8d9d94ead63894b6
Poya Faraji <poya.faraji@outlook.com>
2026-08-03 23:19:49 +0330

    [ADD] Button styles to link

--- src/components/header/header-component.tsx
+++ src/components/header/header-component.tsx
@@ -4,16 +4,28 @@ import Link from "next/link";
 
 import { homePath, ticketsPath } from "@/utils/dynamic-path";
 
+import { buttonVariants } from "../ui/button";
+
 export default function HeaderComponent(): ReactNode {
   return (
     <header className="px-5 py-4 shadow-sm border-b border-border bg-background">
       <nav className="w-[min(1200px,calc(100%-2rem))] mx-auto">
         <ul className="flex justify-between">
-          <li className="font-bold hover:text-primary">
-            <Link href={homePath()}>Home</Link>
+          <li>
+            <Link
+              className={buttonVariants({ variant: "outline" })}
+              href={homePath()}
+            >
+              Home
+            </Link>
           </li>
-          <li className="underline hover:text-primary">
-            <Link href={ticketsPath()}>Tickets</Link>
+          <li>
+            <Link
+              className={buttonVariants({ variant: "outline" })}
+              href={ticketsPath()}
+            >
+              Tickets
+            </Link>
           </li>
         </ul>
       </nav>
