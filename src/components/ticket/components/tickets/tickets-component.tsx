5de87e50e854062cb97a05fbac8f46be507445d5
Poya Faraji <poya.faraji@outlook.com>
2026-08-04 23:50:59 +0330

    [REFACTOR] ticket-component as a server component

--- src/components/ticket/components/tickets/tickets-component.tsx
+++ src/components/ticket/components/tickets/tickets-component.tsx
@@ -1,39 +1,11 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 import TicketItemComponent from "@/components/ticket/components/ticket-item/ticket-item-components";
-import { SpinnerCustom } from "@/components/ui/spinner";
 
 import { getTicket } from "@/services/ticket/ticket-service";
-import { TicketType } from "@/types/ticket-types";
 
-export default function TicketsComponent() {
-  const [tickets, setTickets] = useState<TicketType[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+export default async function TicketsComponent() {
+  const tickets = await getTicket();
 
-  useEffect(() => {
-    const fetchTickets = async () => {
-      const result = await getTicket();
-
-      setTickets(result);
-      setLoading(false);
-    };
-
-    fetchTickets();
-  }, []);
-
-  return (
-    <>
-      {loading ? (
-        <span className="flex justify-center items-center gap-x-4">
-          Loading <SpinnerCustom />
-        </span>
-      ) : (
-        tickets.map((ticket) => {
-          return <TicketItemComponent key={ticket.id} ticket={ticket} />;
-        })
-      )}
-    </>
-  );
+  return tickets.map((ticket) => {
+    return <TicketItemComponent key={ticket.id} ticket={ticket} />;
+  });
 }
