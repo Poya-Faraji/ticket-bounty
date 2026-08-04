4fd2c0a00830ca1f279f7832ae308f6fd10f89bf
Poya Faraji <poya.faraji@outlook.com>
2026-08-04 16:53:08 +0330

    [UPDATE] heading component

--- src/components/heading/heading-component.tsx
+++ src/components/heading/heading-component.tsx
@@ -1,20 +1,17 @@
 import { Separator } from "@/components/ui/separator";
 
-
 type Props = {
-    title : string;
-    description: string;
-}
+  title: string;
+  description: string;
+};
 
-export default function HeadingComponent({title, description}: Props) {
+export default function HeadingComponent({ title, description }: Props) {
   return (
     <>
       <div className="mt-8 w-[min(1200px,calc(100%-2rem))] mx-auto">
-        <div className="pb-8">
+        <div className="ml-4 pb-8">
           <h2 className="font-heading text-2xl font-bold">{title}</h2>
-          <p className="text-muted-foreground">
-            {description}
-          </p>
+          <p className="text-muted-foreground">{description}</p>
         </div>
         <Separator className="mx-auto w-[90%]" />
       </div>
