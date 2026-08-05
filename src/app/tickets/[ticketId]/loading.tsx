b53f72699db5134f57a9f7694a6e59f1940b6213
Poya Faraji <poya.faraji@outlook.com>
2026-08-05 22:34:08 +0330

    [ADD] Loading

---
+++ src/app/tickets/[ticketId]/loading.tsx
@@ -0,0 +1,9 @@
+import { SpinnerCustom } from "@/components/ui/spinner";
+
+export default function Loading() {
+  return (
+    <div className="mt-8">
+      <SpinnerCustom>Loading...</SpinnerCustom>
+    </div>
+  );
+}
