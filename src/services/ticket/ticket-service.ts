7baaa4774673b27282ce940580d59cb263ab4e49
Poya Faraji <poya.faraji@outlook.com>
2026-08-12 16:45:26 +0330

    [SORT] tickets based on updatedAt

--- src/services/ticket/ticket-service.ts
+++ src/services/ticket/ticket-service.ts
@@ -3,7 +3,11 @@ import { Ticket } from "@/generated/prisma/client";
 import { prisma } from "@/lib/prisma";
 
 export const getTickets = async (): Promise<Ticket[]> => {
-  const tickets = await prisma.ticket.findMany({});
+  const tickets = await prisma.ticket.findMany({
+    orderBy: {
+      updatedAt: "desc"
+    },
+  });
 
   return new Promise((resolve) => {
     resolve(tickets);
