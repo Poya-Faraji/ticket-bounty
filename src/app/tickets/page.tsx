26b653f98240b3c34756a0e7b4faa024fc465b48
Poya Faraji <poya.faraji@outlook.com>
2026-08-12 17:05:37 +0330

    [CREATE] Card Compact Component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,17 +2,11 @@ import { type ReactNode, Suspense } from "react";
 
 import { ErrorBoundary } from "react-error-boundary";
 
+import { CardCompactComponent } from "@/components/card-compact/card-compact-component";
 import HeadingComponent from "@/components/heading/heading-component";
 import PlaceholderComponent from "@/components/placeholder/placholder-component";
 import { TicketCreateForm } from "@/components/ticket/components/ticket-create-form/ticket-create-form";
 import TicketsListComponent from "@/components/ticket/components/tickets-list/tickets-list-component";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { SpinnerCustom } from "@/components/ui/spinner";
 
 export default function Page(): ReactNode {
@@ -27,15 +21,13 @@ export default function Page(): ReactNode {
       />
 
       <div className="flex-1 flex flex-col mt-8">
-        <Card className="w-full max-w-105 self-center">
-          <CardHeader>
-            <CardTitle>Create Ticket</CardTitle>
-            <CardDescription>A new ticket will be created</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <TicketCreateForm />
-          </CardContent>
-        </Card>
+        <CardCompactComponent
+          title="Create Ticket"
+          description="A new ticket will be created"
+          className="w-full max-w-105 self-center"
+        >
+          <TicketCreateForm />
+        </CardCompactComponent>
       </div>
 
       <div className="mt-8 mx-auto max-w-80 grid grid-cols-1 gap-5">
---
+++ src/components/card-compact/card-compact-component.tsx
@@ -0,0 +1,39 @@
+import { PropsWithChildren, ReactNode } from "react";
+
+import { TicketCreateForm } from "@/components/ticket/components/ticket-create-form/ticket-create-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+type Props = PropsWithChildren & {
+  className?: string;
+  title: string;
+  description: string;
+  footer?: ReactNode;
+};
+const CardCompactComponent = ({
+  className,
+  title,
+  description,
+  footer,
+  children,
+}: Props): ReactNode => {
+  return (
+    <Card className={className}>
+      <CardHeader>
+        <CardTitle>{title}t</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+
+      {footer && <CardFooter>{footer}</CardFooter>}
+    </Card>
+  );
+};
+
+export { CardCompactComponent };
